aeb7b9f3ea5247d9d0aedb8cbd7d479e79abf474
jpcruz1020 <jdossantoscruz@torontomu.ca>
2024-11-19 13:47:22 -0500

    Update PID_DC.ino
    Added HMI and Error Conditions

--- PID_DC.ino
+++ PID_DC.ino
@@ -5,10 +5,23 @@
 #define A 2
 #define B 3
 
+//Assign LEDs
+#define LED_POWER 4
+#define LED_PID 5
+#define LED_POLE 6
+
+//Assign switches
+#define TOGGLE 7
+#define ESTOP 8
+
 //Assign DC Motor pins
-#define D1 8
-#define D2 9
-#define PWM 10
+#define D1 9
+#define D2 10
+#define PWM 11
+
+//Assign Ultrasonic Sensor
+#define TRIG 12
+#define ECHO 13
 
 //Assign DC Motor Max and Min Outputs
 #define MAX_MOTOR 50
@@ -33,6 +46,19 @@ pinMode(D1, OUTPUT);
 pinMode(D2, OUTPUT);
 pinMode(PWM, OUTPUT);
 
+//Initialzie LEDs
+pinMode(LED_POWER, OUTPUT);
+pinMode(LED_PID, OUTPUT);
+pinMode(LED_POLE, OUTPUT);
+
+//Initialzie Switches
+pinMode(TOGGLE, INPUT);
+pinMode(ESTOP, INPUT);
+
+//Initialzie Ultrasonic Sensor
+pinMode(TRIG, OUTPUT);
+pinMode(ECHO, INPUT);
+
 //Initialize PID and set output limits
 sysPID.SetMode(AUTOMATIC);
 sysPID.SetOutputLimits(-5,5);
@@ -40,6 +66,19 @@ sysPID.SetOutputLimits(-5,5);
 }
 
 void loop() {
+//Check ESTOP State
+if(ESTOP == LOW){
+
+  //Check TOGGLE State
+  while(TOGGLE == HIGH){
+//Measure cart postion using ultrasonic sensor
+double distance = DistMeas();
+
+if((distance <= 40 || distance >= 10) && (PendAngle <= -45 || PendAngle >= 45)){
+//Turn on LEDs
+digitalWrite(LED_POWER, HIGH);
+digitalWrite(LED_PID, HIGH);
+
 //Read Encoder value and convert to degrees
 long count = Enc.read();
 PendAngle = count * 0.15;
@@ -71,5 +110,32 @@ else {
     analogWrite(PWM, 0);
   }
 delay(50);
+}
 
+else {
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);
+  analogWrite(PWM, 0);
+  return;
+}
+    }
+
+}
+
+else{
+//Break from loop
+return;
+}
+}
+
+int DistMeas() {
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(3);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  
+  int duration = pulseIn(ECHO, HIGH);
+  int dist = duration * 0.034 / 2;
+  return  dist;
 }
